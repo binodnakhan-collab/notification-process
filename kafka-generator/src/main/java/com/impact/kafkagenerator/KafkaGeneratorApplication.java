542df68bcb48ecbd8aab20c7eaabfa6222e21d2d
Binod Nakhan <binod.nakhan@swifttech.com>
2026-01-14 15:33:50 +0545

    initial pproject setup

---
+++ kafka-generator/src/main/java/com/impact/kafkagenerator/KafkaGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.impact.kafkagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaGeneratorApplication.class, args);
+    }
+
+}
---
+++ kafka-generator/src/test/java/com/impact/kafkagenerator/KafkaGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.kafkagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
